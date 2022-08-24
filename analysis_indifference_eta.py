e45d88459f048d096aac402d382b755b11e7f030
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2022-08-24 15:44:46 +0100

    minor clean up

--- analysis_indifference_eta.py
+++ analysis_indifference_eta.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv(os.path.join(os.path.dirname(__file__),'data','all_data.csv'), sep='\t')
+root_path = os.path.dirname(__file__)
+
+df = pd.read_csv(os.path.join(root_path,'data','all_data.csv'), sep='\t')
 
 ###create indifference eta and wealth plots
 fig_dynamic, ax_dynamic = plt.subplots(1,len(set(df.eta)),figsize = (12,5))
@@ -29,26 +31,21 @@ for dynamic_idx, dynamic in enumerate(set(df.eta)):
             ax_subject[subject_idx,dynamic_idx].plot(trial.indif_eta, ii, marker=trial.min_max_sign, color = trial.min_max_color)
             ax_dynamic[dynamic_idx].plot(trial.indif_eta, trial_counter, marker=trial.min_max_sign, color = trial.min_max_color)
 
-            ax_subject[subject_idx,dynamic_idx].set_title(f'Subject {subject}, Dynamic {dynamic}')
-            ax_subject[subject_idx,dynamic_idx].set_xlabel('Riskaversion ($\eta$)')
+            ax_subject[subject_idx,dynamic_idx].set(title = f'Subject {subject}, Dynamic {dynamic}', xlabel = 'Riskaversion ($\eta$)')
             ax_subject[subject_idx,dynamic_idx].axes.yaxis.set_visible(False)
             ax_subject[subject_idx,dynamic_idx].axvline(dynamic, linestyle='--', linewidth=1, color='k')
 
         ax_subject_wealth[subject_idx,dynamic_idx].plot(df_tmp.index,df_tmp.wealth)
-        ax_subject_wealth[subject_idx,dynamic_idx].set_title(f'Subject {subject} wealth, Dynamic {dynamic}')
-        ax_subject_wealth[subject_idx,dynamic_idx].set_xlabel('Trial')
-        ax_subject_wealth[subject_idx,dynamic_idx].set_ylabel('w')
+        ax_subject_wealth[subject_idx,dynamic_idx].set(title = f'Subject {subject} wealth, Dynamic {dynamic}', xlabel = 'Trial', ylabel = 'w')
 
         ax_dynamic[dynamic_idx].axhline(trial_counter, linestyle='--', linewidth=1, color='k')
         tick_place.append(trial_counter - trial_counter_participant / 2)
     if dynamic_idx == 0:
-        ax_dynamic[dynamic_idx].set_yticks(tick_place[1:])
-        ax_dynamic[dynamic_idx].set_yticklabels(tick_names, minor=False)
+        ax_dynamic[dynamic_idx].set(yticks = tick_place[1:], yticklabels = tick_names, minor=False)
     else:
         ax_dynamic[dynamic_idx].axes.yaxis.set_visible(False)
 
-    ax_dynamic[dynamic_idx].set_xlabel('Riskaversion ($\eta$)')
-    ax_dynamic[dynamic_idx].set_title(dynamic)
+    ax_dynamic[dynamic_idx].set(title = dynamic, xlabel = 'Riskaversion ($\eta$)')
     ax_dynamic[dynamic_idx].axvline(dynamic, linestyle='--', linewidth=1, color='k')
 plt.tight_layout()
 plt.show()
