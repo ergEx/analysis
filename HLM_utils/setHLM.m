bc02e15757e8c3c61f3e5e791164932fe14c693a
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2022-08-25 12:00:21 +0100

    Including HLM modelling

--- HLM_utils/setHLM.m
+++ HLM_utils/setHLM.m
@@ -1,14 +1,15 @@
 function setHLM(runModelNum,whichJAGS,whichQuals,doParallel,startDir)
+
 % setHLM sets up multiple HLM models to run sequentially according to inputs
 
 % This function takes the following inputs:
 
-% runModelNum - which models to run: (1) parameter estimation
-% synthMode   - sets whether to run on real data (1), synthetic data for parameter recovery (2-5; see setHLM.m for info on differences)
-% nDynamics   - number of dynamics tested
+% runModelNum - set which model to run; parameter estimation (1) or model selection (2),
+% synthMode   - sets whether to run on real data (1) or synthetic data for parameter recovery (2-3; see below for info on differences)
 % whichJAGS   - which copy of matjags to run on. this allows parallel jobs to run as long as they use different matjags
 % whichQuals  - sets the order of qualities to run
 % doParallel  - whether to run chains in parallel
+% startDir    - root directory for the repo
 
 %% Specifies qualities to be selected from
 numRuns      = length(whichQuals);     %how many separate instances of an MCMC to run
@@ -18,9 +19,11 @@ nChains      = [4,4,4,4,4];            %Keep this to 4
 nThin        = 10;                     %thinnning factor, 1 = no thinning, 2=every 2nd etc.
 
 %% Specifies subjects
-subjList  = [1:4,6:19];  %the subjects from the experiment (excludes 5 who didnt learn the stimuli)
+subjList{1}  = 1:2;     %the subjects from the experiment
+subjList{2}  = 1:2;     %synthetic agents for model recovery
+subjList{3}  = 1:;      %synthetic agents for parameter recover, 9 equally spaced agents forming a 3x3 grid in eta space
 
 %% Runs HLMs sequentially
 for i=1:numRuns
-    computeHLM(runModelNum,synthMode,nDynamics,nBurnin(whichQuals(i)),nSamples(whichQuals(i)),nThin,nChains(whichQuals(i)),subjList,whichJAGS,doParallel,startDir)
+    computeHLM(runModelNum,nBurnin(whichQuals(i)),nSamples(whichQuals(i)),nThin,nChains(whichQuals(i)),subjList,whichJAGS,doParallel,startDir)
 end
