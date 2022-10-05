9f9e6b0ae33436e011f4143bf911f1a553a1d2bf
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2022-10-05 14:48:09 +0100

    main analysis scrips; indifference eta plots added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 RESET = 45
@@ -25,7 +26,7 @@ for c,condition in enumerate(set(passive_phase_df['eta'])):
         passive_subject_df = passive_phase_df.query('participant_id == @subject and eta == @condition').reset_index(drop=True)
         fig, ax = plt.subplots(1,1)
         ax.plot(passive_subject_df.trial, passive_subject_df.wealth)
-        ax.set(title=f'Passive phase, Subject {subject}, Condition: {condition_specs[condition]}',
+        ax.set(title=f'Passive phase \nSubject {subject}, Condition: {condition_specs[condition]}',
                xlabel='Trial',
                ylabel='Wealth')
         for reset_idx in range(1,4):
@@ -33,5 +34,32 @@ for c,condition in enumerate(set(passive_phase_df['eta'])):
         ax.plot([], label="Reset",color='grey',linestyle='--')
         ax.legend()
 
-        plt.savefig(os.path.join(save_path, f'Passive_trajectory_{condition_specs[condition]}.png'))
+        fig.savefig(os.path.join(save_path, f'Passive_trajectory_{condition_specs[condition]}.png'))
+        plt.close(fig)
+
+        '''ACTIVE PHASE'''
+        active_subject_df = active_phase_df.query('participant_id == @subject and eta == @condition').reset_index(drop=True)
+
+
+        #Indifference eta plots
+        fig, ax = plt.subplots(1,1)
+        for ii, choice in enumerate(active_subject_df['selected_side_map']):
+            trial = active_subject_df.loc[ii,:]
+            if np.isnan(trial.indif_eta):
+                continue
+
+            ax.axvline(condition, linestyle='--', linewidth=1, color='k')
+
+            ax.plot(trial.indif_eta, ii, marker=trial.min_max_sign, color=trial.min_max_color)
+
+            ax.set(title = f'Indifference eta \nSubject {subject}, Condition: {condition_specs[condition]}',
+                   xlabel = 'Riskaversion ($\eta$)')
+
+            ax.axes.yaxis.set_visible(False)
+
+        fig.savefig(os.path.join(save_path, f'Indifference_eta_{condition_specs[condition]}.png'))
+        ax.set_xlim([-2,3])
+        fig.savefig(os.path.join(save_path, f'Indifference_eta_zoom_{condition_specs[condition]}.png'))
+        plt.close(fig)
+
 
