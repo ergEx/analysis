c5ed1504cf8962ad32e94b97afcbc3d4315613cf
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2022-11-25 13:42:40 +0000

    main include aggregation to super-participant

--- main.py
+++ main.py
@@ -48,6 +48,9 @@ if not os.path.isdir(save_path):
 
 logistic_regression_input = np.empty([len(subjects),7])
 
+full_data = {0.0: {'indif_eta': [], 'min_max_sign': [], 'min_max_color':[], 'min_max_val':[]},
+             1.0: {'indif_eta': [], 'min_max_sign': [], 'min_max_color':[], 'min_max_val':[]}}
+
 
 
 for i,subject in enumerate(subjects):
@@ -98,6 +101,10 @@ for i,subject in enumerate(subjects):
             trial = active_subject_df.loc[ii,:]
             if np.isnan(trial.indif_eta):
                 continue
+            full_data[condition]['indif_eta'].append(trial.indif_eta)
+            full_data[condition]['min_max_sign'].append(trial.min_max_sign)
+            full_data[condition]['min_max_color'].append(trial.min_max_color)
+            full_data[condition]['min_max_val'].append(trial.min_max_val)
             ax[c,2].plot(trial.indif_eta, ii, marker=plot_specs['sign'][trial.min_max_sign], color=plot_specs['color'][trial.min_max_color])
         ax[c,2].set(title = f'Indifference eta',
                     xlabel = 'Riskaversion ($\eta$)')
@@ -184,3 +191,68 @@ else:
     logistic_regression_outout.to_csv(os.path.join(root_path,'data',design_variant, 'logistic_regression_output.csv'), sep='\t')
 
 
+#### TREAT ALL AGENTS AS 1
+
+fig, ax = plt.subplots(2,3, figsize=(20,12))
+for c,condition in enumerate(condition_specs.keys()):
+    df = pd.DataFrame.from_dict(full_data[condition])
+
+    #Indifference eta plots
+    plot_specs = {'color':{0:'orange', 1: 'b'}, 'sign':{0:'>', 1:'<'}}
+    for ii in range(len(df)):
+        trial = df.loc[ii,:]
+        ax[c,0].plot(trial.indif_eta, ii, marker=plot_specs['sign'][trial.min_max_sign], color=plot_specs['color'][trial.min_max_color])
+        ax[c,0].set(title = f'Indifference eta',
+                    xlabel = 'Riskaversion ($\eta$)')
+    ax[c,0].axes.yaxis.set_visible(False)
+    ax[c,0].axvline(condition, linestyle='--', color='grey', label='Growth optimal')
+
+    #Choice probabilities in different indifference eta regions
+    #min_df = df.query('min_max_sign == 0').reset_index(drop=True)
+    #max_df = df.query('min_max_sign == 1').reset_index(drop=True)
+    #min, _ = np.histogram(min_df['indif_eta'], bins=[-np.inf, -0.5, 0, 1.0, 1.5, np.inf])
+    #max, _ = np.histogram(max_df['indif_eta'], bins=[-np.inf, -0.5, 0, 1.0, 1.5, np.inf])
+    #h = [max[i]/(max[i]+min[i]) for  i in range(len(min))]
+    #ticks = ['<-0.5','-1 - 0','0 - 1','1 - 1.5','>1.5']
+    #ax[c,1].bar(ticks,h)
+    #ax[c,1].set(title=f'Indif eta choice prob.',
+    #            ylim=[0,1],
+    #            yticks=np.linspace(0,1,11))
+    #ax[c,1].tick_params(axis='x', labelrotation=45)
+
+    #Indifference eta logistic regression
+
+    df_tmp = df.query('indif_eta.notnull()', engine='python')
+    print(f'Number og relevant gambles: {len(df_tmp) / len(df):.2f}')
+
+    x_test, pred, ymin, ymax, idx_m, idx_l, idx_h = logistic_regression(df_tmp)
+
+
+    ax[c,1].fill_between(x_test, ymin, ymax, where=ymax >= ymin,
+                facecolor='grey', interpolate=True, alpha=0.5,label='95 % confidence interval')
+
+    ax[c,1].plot(x_test,pred,color='black')
+
+    sns.regplot(x=np.array(df_tmp.indif_eta),
+                y=np.array(df_tmp.min_max_val),
+                fit_reg=False,
+                y_jitter=0.05,
+                ax=ax[c,1],
+                label='data',
+                color='grey',
+                scatter_kws={'alpha': 1, 's':20})
+
+    ax[c,1].axhline(y=0.5,color='grey',linestyle='--')
+
+    ax[c,1].axvline(x=x_test[idx_m], color='grey', linestyle='--')
+
+    ax[c,1].set(title=f'Logistic regression',
+        ylabel = '',
+        xlabel = 'Indifference eta',
+        yticks = [0, 0.5, 1],
+        ylim = (-0.25, 1.25),
+        xticks = np.linspace(-5,5,11),
+        xlim = [-5,5])
+
+fig.tight_layout()
+fig.savefig(os.path.join(save_path, f'active_results_new.png'))
