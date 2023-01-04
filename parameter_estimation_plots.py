61dcba107b22243ad2a95073b0e87ef934833e47
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2023-01-04 15:00:35 +0100

    major refactoring of reading_data and plotting parameter estimates

---
+++ parameter_estimation_plots.py
@@ -0,0 +1,267 @@
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from utils import logistic_regression, read_hlm_output
+
+
+def plot_passive_trajectory(df:pd.DataFrame, ax:plt.axis, n_passive_runs:int, reset:int, c:int, idx:int=0) -> plt.axis:
+    df = df.query('participant_id == @subject and eta == @condition').reset_index(drop=True)
+    ax[c,idx].plot(df.trial, df.wealth)
+    for reset_idx in range(1,n_passive_runs):
+        ax[c,0].axvline(x=reset*reset_idx,color='grey',linestyle='--')
+    ax[c,idx].plot([], label="Reset",color='grey',linestyle='--')
+    ax[c,idx].legend(loc='upper left', fontsize='xx-small')
+    ax[c,idx].set(title=f'Passive wealth',
+                xlabel='Trial',
+                ylabel=f'Wealth')
+    if c == 1:
+        ax[c,idx].set_yscale('log')
+
+def plot_active_trajectory(df:pd.DataFrame, ax:plt.axis, active_limits:dict,c:int,idx:int=1) -> plt.axis:
+    ax[c,idx].plot(df.trial, df.wealth)
+    ax[c,idx].set(title=f'Active wealth',
+                xlabel='Trial',
+                ylabel='Wealth')
+
+    ax[c,idx].axhline(y=active_limits[c][0], linestyle='--', linewidth=1,color='red', label='Upper Bound')
+    ax[c,idx].axhline(y=1000, linestyle='--', color='black', label='Starting Wealth')
+    ax[c,idx].axhline(y=active_limits[c][1], linestyle='--', linewidth=1, color='red', label='Lower Bound')
+    ax[c,idx].legend(loc='upper left', fontsize='xx-small')
+    if c == 1:
+        ax[c,1].set(yscale='log',
+                    ylabel='Wealth (log)')
+
+def plot_indifference_eta(df:pd.DataFrame,ax:plt.axis,plot_specs:dict,c:int,simulation_eta:float=None,idx:int=2):
+    for ii, choice in enumerate(df['selected_side_map']):
+        trial = df.loc[ii,:]
+        if np.isnan(trial.indif_eta):
+            continue
+        ax[c,idx].plot(trial.indif_eta, ii, marker=plot_specs['sign'][trial.min_max_sign], color=plot_specs['color'][trial.min_max_color])
+
+    ax[c,idx].set(title = f'Indifference eta',
+                xlabel = 'Riskaversion ($\eta$)')
+    ax[c,idx].axes.yaxis.set_visible(False)
+    ax[c,idx].axvline(c, linestyle='--', color='grey', label='Growth optimal')
+    if simulation_eta is not None:
+        ax[c,idx].axvline(simulation_eta, linestyle='--', color='green', label = 'Simulation eta')
+
+    ax[c,idx].plot([],marker='<',color='b', label='Upper bound')
+    ax[c,idx].plot([],marker='>',color='orange', label='Lower bound')
+
+    ax[c,idx].legend(loc='upper left', fontsize='xx-small')
+
+def plot_choice_probabilities(df:pd.DataFrame,ax:plt.axis,c:int,idx:int=3):
+    bins = [-np.inf, -0.5, 0, 1.0, 1.5, np.inf]
+    min_df = df[df['min_max_sign'] == 0]
+    max_df = df[df['min_max_sign'] == 1]
+    min_count, _ = np.histogram(min_df['indif_eta'], bins=bins)
+    max_count, _ = np.histogram(max_df['indif_eta'], bins=bins)
+    choice_probs = [max_count[i] / (max_count[i] + min_count[i]) for i in range(len(min_count))]
+    ticks = ['<-0.5', '-1 - 0', '0 - 1', '1 - 1.5', '>1.5']
+
+    ax[c, idx].bar(ticks, choice_probs)
+    ax[c, idx].set(title='Indif eta choice prob.', ylim=[0, 1], yticks=np.linspace(0, 1, 11))
+    ax[c, idx].tick_params(axis='x', labelrotation=45)
+
+def plot_indif_eta_logistic_reg(df:pd.DataFrame, ax:plt.axis, c:int,simulation_eta:float=None, idx:int=4):
+    #Indifference eta logistic regression
+    df_tmp = df.query('indif_eta.notnull()', engine='python')
+    df_tmp_1 = df_tmp[df_tmp['min_max_val'] == 1]
+    df_tmp_0 = df_tmp[df_tmp['min_max_val'] == 0]
+
+    print(f'Number og relevant gambles: {len(df_tmp) / len(df):.2f}')
+    x_test, pred, ymin, ymax, idx_m, idx_l, idx_h = logistic_regression(df_tmp)
+
+
+    ax[c,idx].fill_between(x_test, ymin, ymax, where=ymax >= ymin,
+                        facecolor='grey', interpolate=True, alpha=0.5,label='95 % CI')
+
+    ax[c,idx].plot(x_test,pred,color='black')
+
+    sns.regplot(x=np.array(df_tmp_1.indif_eta),
+                y=np.array(df_tmp_1.min_max_val),
+                fit_reg=False,
+                y_jitter=0.05,
+                ax=ax[c,idx],
+                label='Upper Bound',
+                color='b',
+                marker='<',
+                scatter_kws={'alpha': 1, 's':20})
+    sns.regplot(x=np.array(df_tmp_0.indif_eta),
+                y=np.array(df_tmp_0.min_max_val),
+                fit_reg=False,
+                y_jitter=0.05,
+                ax=ax[c,idx],
+                label='Lower Bound',
+                color='orange',
+                marker='>',
+                scatter_kws={'alpha': 1, 's':20})
+
+    ax[c,idx].axvline(c, linestyle='--', color='grey', label='Growth optimal')
+    ax[c,idx].axhline(y=0.5,color='grey',linestyle='--')
+
+    ax[c,idx].set(title=f'Logistic regression',
+        ylabel = '',
+        xlabel = 'Indifference eta',
+        yticks = [0, 0.5, 1],
+        ylim = (-0.25, 1.25),
+        xticks = np.linspace(-5,5,11),
+        xlim = [-5,5])
+    if simulation_eta is not None:
+        ax[c,idx].axvline(simulation_eta, linestyle='--', color='green', label='Simulation eta')
+    ax[c,idx].axvline(x=x_test[idx_m], color='red', linestyle='--', label = 'Best estimate')
+    ax[c,idx].legend(loc='upper left', fontsize='xx-small')
+
+def plot_bayesian_estimation(dist:np.array,ax:plt.axis,c:int,simulation_eta:float=None,idx:int=5):
+    sns.kdeplot(dist,ax=ax[c,idx])
+    xs, ys = ax[c,idx].lines[-1].get_data()
+    ax[c,idx].fill_between(xs, ys, color='red', alpha=0.05)
+    mode_idx = np.argmax(ys)
+    ax[c,idx].vlines(xs[mode_idx], 0, ys[mode_idx], ls='--', color='red', label='Prediction')
+    ax[c,idx].axvline(c, linestyle='--', linewidth=1, color='k')
+    ax[c,idx].set(title=f'Bayesian Model',
+        ylabel = '',
+        xticks = np.linspace(-5,5,11),
+        xlim = [-5,5],
+        xlabel='Risk aversion estimate')
+
+    if simulation_eta is not None:
+        ax[c,idx].axvline(simulation_eta, linestyle='--', color='green', label='Simulation eta')
+
+    ax[c,idx].legend(loc='upper left', fontsize='xx-small')
+
+def plot_subject_wise(save_path:str,
+                      subjects:list[str],
+                      condition_specs:dict,
+                      simulation:bool,
+                      passive_phase_df:pd.DataFrame,
+                      n_passive_runs:int,
+                      reset:int,
+                      active_phase_df:pd.DataFrame,
+                      active_limits:dict,
+                      indifference_eta_plot_specs:dict,
+                      bayesian_samples:np.array=None):
+    for i,subject in enumerate(subjects):
+        print(f'Subject {i+1} of {len(subjects)}')
+
+        fig, ax = plt.subplots(2,6, figsize=(20,7))
+        fig.suptitle(f'Subject {subject}')
+        for c,condition in enumerate(condition_specs.keys()):
+            print(f'Condition {c+1} of {len(condition_specs)}')
+
+            '''PASIVE PHASE'''
+            if simulation:
+                ax[c,0].plot()
+            else:
+                plot_passive_trajectory(passive_phase_df,ax, n_passive_runs, reset, c)
+
+            '''ACTIVE PHASE'''
+            print(subject, condition)
+            if simulation:
+                active_subject_df = active_phase_df.query('agent == @subject and eta == @condition').reset_index(drop=True)
+                simulation_eta = float(subject.split('x')[c])
+            else:
+                active_subject_df = active_phase_df.query('no_response != True and subject_id == @subject and eta == @condition').reset_index(drop=True)
+
+            plot_active_trajectory(active_subject_df,ax, active_limits, c)
+
+            plot_indifference_eta(active_subject_df,ax,indifference_eta_plot_specs,c,simulation_eta)
+
+            plot_choice_probabilities(active_subject_df,ax,c)
+
+            plot_indif_eta_logistic_reg(active_subject_df,ax,c,simulation_eta)
+
+            if bayesian_samples is not None:
+                ax[c,5].plot()
+            else:
+                eta_dist = bayesian_samples['eta'][:,:,i,c].flatten()
+                plot_bayesian_estimation(eta_dist,ax,c,simulation_eta)
+
+        fig.tight_layout()
+        fig.savefig(os.path.join(save_path, f'Subject {subject}.png'))
+
+def plot_all_data_as_one(save_path:str,
+                      condition_specs:dict,
+                      simulation:bool,
+                      df:pd.DataFrame,
+                      indifference_eta_plot_specs:dict,
+                      bayesian_samples:np.array=None):
+    fig, ax = plt.subplots(2,3, figsize=(20,12))
+    fig.suptitle(f'All data')
+    for c,condition in enumerate(condition_specs.keys()):
+        print(f'Condition {c+1} of {len(condition_specs)}')
+        if simulation:
+            df_c = df.query('eta == @condition').reset_index(drop=True)
+        else:
+            df_c = df.query('no_response != True and eta == @condition').reset_index(drop=True)
+        plot_indifference_eta(df_c,ax,indifference_eta_plot_specs,c,idx=0)
+
+        plot_indif_eta_logistic_reg(df_c,ax,c,simulation,idx=1)
+
+        if bayesian_samples is not None:
+            ax[c,1].plot()
+        else:
+            eta_dist = bayesian_samples['eta'][:,:,:,c].flatten()
+            plot_bayesian_estimation(eta_dist,ax,c,idx=2)
+
+        fig.tight_layout()
+        fig.savefig(os.path.join(save_path, f'active_results_aggregated.png'))
+
+if __name__=='__main__':
+    simulation = True
+    reset = 45
+    n_passive_runs = 3
+    root_path = os.path.dirname(__file__)
+    design_variant = 'two_gamble_new_c'
+    inference_mode = 'parameter_estimation'
+
+    condition_specs = {0.0:'Additive', 1.0:'Multiplicative'}
+    active_limits = {0.0: [-500, 2_500], 1.0: [64 , 15_589]}
+    indifference_eta_plot_specs = {'color':{0:'orange', 1: 'b'}, 'sign':{0:'>', 1:'<'}}
+
+    if simulation:
+        passive_phase_df = None
+        active_phase_df = pd.read_csv(os.path.join(root_path,'data',design_variant,'simulations','all_active_phase_data.csv'), sep='\t')
+        subjects = ['0.0x0.0','0.0x0.5','0.0x1.0','0.5x0.0','0.5x0.5','0.5x1.0','1.0x0.0','1.0x0.5','1.0x1.0']
+        data_source = 'simulated_data'
+        save_path = os.path.join(root_path, 'figs', design_variant, 'simulations')
+    else:
+        passive_phase_df = pd.read_csv(os.path.join(root_path,'data',design_variant,'all_passive_phase_data.csv'), sep='\t')
+        active_phase_df = pd.read_csv(os.path.join(root_path,'data',design_variant,'all_active_phase_data.csv'), sep='\t')
+        subjects = set(passive_phase_df['participant_id'])
+        data_source = 'real_data'
+        save_path = os.path.join(root_path, 'figs', design_variant)
+
+    bayesian_output_file = os.path.join(root_path,'data',design_variant,f'{inference_mode}_{data_source}.mat')
+    if os.path.isfile(bayesian_output_file):
+        bayesian_samples = read_hlm_output(inference_mode = inference_mode, experiment_version = design_variant, data_source = data_source)
+    else:
+        bayesian_samples = None
+        print('HLM model output not found!')
+
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+
+    plot_subject_wise(save_path,
+                     subjects,
+                     condition_specs,
+                     simulation,
+                     passive_phase_df,
+                     n_passive_runs,
+                     reset,
+                     active_phase_df,
+                     active_limits,
+                     indifference_eta_plot_specs,
+                     bayesian_samples)
+
+    plot_all_data_as_one(save_path,
+                      condition_specs,
+                      simulation,
+                      active_phase_df,
+                      indifference_eta_plot_specs,
+                      bayesian_samples)
--- readingdata.py
+++ readingdata.py
@@ -7,77 +7,95 @@ import scipy.io
 import sub_specs
 import utils
 
-print('READING DATA')
-simulation = True
-root_path = os.path.join(os.path.dirname(__file__),)
-design_variant = 'two_gamble_new_c'
-condition_specs = {'condition':['Additive','Multiplicative'], 'lambd':[0.0,1.0], 'bids_text': ['0d0','1d0'],'txt_append':['_add','_mul']}
-if simulation:
-    subject_specs = {'id': ['0.0x0.0','0.0x0.5','0.0x1.0','0.5x0.0','0.5x0.5','0.5x1.0','1.0x0.0','1.0x0.5','1.0x1.0']}
-else:
-    subject_specs = sub_specs.sub_specs(design_variant)
-n_passive_runs = 3
 
-passive_phase_df = pd.DataFrame()
-active_phase_df = pd.DataFrame()
-datadict = dict()
-for c,condition in enumerate(condition_specs['condition']):
-    print(f'Condition {c+1} of {len(condition_specs["lambd"])}')
-    for i,subject in enumerate(subject_specs['id']):
-        print(f'Subject {i+1} of {len(subject_specs["id"])}')
-        '''Passive phase data'''
-        if not simulation:
-            for run in range(1,n_passive_runs+1):
-                passive_phase_data = pd.read_csv(os.path.join(root_path, 'data',design_variant,f'sub-{subject}',f'ses-{subject_specs["first_run"][i][c]}',f'sub-{subject}_ses-{subject_specs["first_run"][i][c]}_task-passive_acq-lambd{condition_specs["bids_text"][c]}_run-{run}_beh.csv'),sep='\t')
+def reading_data(simulation:bool, design_variant:str, n_passive_runs:int) -> None:
+    """
+    Reads in passive and active phase data for a given design variant, simulation status, and number of passive runs. It stores a tuple of two dataframes, one for passive phase data and one for active phase data. It also stores .mat and .npz versions of the active phase data in a subdirectory within the data directory.
 
-                subject_df = passive_phase_data.query('event_type == "WealthUpdate"').reset_index(drop=True)
-                subject_df = subject_df.query('part == 0').reset_index(drop=True)
-                passive_phase_df = pd.concat([passive_phase_df, subject_df])
+    Parameters:
+    simulation (bool): Whether to read in data from simulations or real experiments.
+    design_variant (str): The design variant of the experiment.
+    n_passive_runs (int): The number of passive runs in the experiment.
+    Returns:
 
-        '''Active phase data'''
-        run = 1
-        if simulation:
-            subject_df = pd.read_csv(os.path.join(root_path, 'data', design_variant, 'simulations', f'sim_agent_{subject}_lambd_{int(condition_specs["lambd"][c])}.csv' ), sep='\t')
-        else:
-            active_phase_data = pd.read_csv(os.path.join(root_path, 'data',design_variant,f'sub-{subject}',f'ses-{subject_specs["first_run"][i][c]}',f'sub-{subject}_ses-{subject_specs["first_run"][i][c]}_task-active_acq-lambd{condition_specs["bids_text"][c]}_run-{run}_beh.csv'),sep='\t')
-            subject_df = active_phase_data.query('event_type == "WealthUpdate"').reset_index(drop=True)
+    one. Data is stored in the relevant subdirectories within the data directory.
+    """
+    print('READING DATA')
 
-        subject_df = utils.add_info_to_df(subject_df, subject)
+    CONDITION_SPECS = {'condition':['Additive','Multiplicative'],
+                       'lambd':[0.0,1.0],
+                       'bids_text': ['0d0','1d0'],
+                       'txt_append':['_add','_mul']}
+    SUBJECT_SPECS = sub_specs.sub_specs(design_variant, simulation)
 
-        ##CSV
-        active_phase_df = pd.concat([active_phase_df, subject_df])
+    if simulation:
+        root_path = os.path.join(os.path.dirname(__file__), 'data', design_variant, 'simulations')
+    else:
+        root_path = os.path.join(os.path.dirname(__file__), 'data', design_variant)
 
-        ##.mat
-        subject_df.loc[np.isnan(subject_df['indif_eta']), 'selected_side_map'] = np.nan
+    passive_phase_df = pd.DataFrame()
+    active_phase_df = pd.DataFrame()
+    datadict = dict()
+    for c,condition in enumerate(CONDITION_SPECS['condition']):
+        print(f'Condition {c+1} of {len(CONDITION_SPECS["lambd"])}')
+        for i,subject in enumerate(SUBJECT_SPECS['id']):
+            print(f'Subject {i+1} of {len(SUBJECT_SPECS["id"])}')
+            '''Passive phase data'''
+            if not simulation:
+                for run in range(1,n_passive_runs+1):
+                    passive_phase_data = pd.read_csv(os.path.join(root_path,f'sub-{subject}',f'ses-{SUBJECT_SPECS["first_run"][i][c]}',f'sub-{subject}_ses-{SUBJECT_SPECS["first_run"][i][c]}_task-passive_acq-lambd{CONDITION_SPECS["bids_text"][c]}_run-{run}_beh.csv'),sep='\t')
 
-        #Retrieve growth rates
-        datadict.setdefault(f'gr1_1{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['gamma_left_up']))
-        datadict.setdefault(f'gr1_2{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['gamma_left_down']))
-        datadict.setdefault(f'gr2_1{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['gamma_right_up']))
-        datadict.setdefault(f'gr2_2{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['gamma_right_down']))
+                    subject_df = passive_phase_data.query('event_type == "WealthUpdate"').reset_index(drop=True)
+                    subject_df = subject_df.query('part == 0').reset_index(drop=True)
+                    passive_phase_df = pd.concat([passive_phase_df, subject_df])
 
-        #Retrieve wealth changes
-        datadict.setdefault(f'x1_1{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['x1_1']))
-        datadict.setdefault(f'x1_2{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['x1_2']))
-        datadict.setdefault(f'x2_1{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['x2_1']))
-        datadict.setdefault(f'x2_2{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['x2_2']))
+            '''Active phase data'''
+            run = 1
+            if simulation:
+                subject_df = pd.read_csv(os.path.join(root_path,f'sim_agent_{subject}_lambd_{int(CONDITION_SPECS["lambd"][c])}.csv'), sep='\t')
+            else:
+                active_phase_data = pd.read_csv(os.path.join(root_path,f'sub-{subject}',f'ses-{SUBJECT_SPECS["first_run"][i][c]}',f'sub-{subject}_ses-{SUBJECT_SPECS["first_run"][i][c]}_task-active_acq-lambd{CONDITION_SPECS["bids_text"][c]}_run-{run}_beh.csv'),sep='\t')
+                subject_df = active_phase_data.query('event_type == "WealthUpdate"').reset_index(drop=True)
 
-        #Retrive wealth
-        datadict.setdefault(f'wealth{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['wealth']))
+            subject_df = utils.add_info_to_df(subject_df)
 
-        #Retrieve keypresses
-        datadict.setdefault(f'choice{condition_specs["txt_append"][c]}',[]).append(np.array(subject_df['selected_side_map']))
+            ##CSV
+            active_phase_df = pd.concat([active_phase_df, subject_df])
 
-if simulation:
-    simulation_path = os.path.join(root_path, 'data',design_variant,'simulations')
-    if not os.path.isdir(simulation_path):
-        os.makedirs(simulation_path)
-    active_phase_df.to_csv(os.path.join(simulation_path,'all_active_phase_data.csv'),sep='\t')
-    scipy.io.savemat(os.path.join(os.path.join(simulation_path,'all_active_phase_data.mat')),datadict,oned_as='row')
-    np.savez(os.path.join(os.path.join(simulation_path,'all_active_phase_data.mat.npz')),datadict = datadict)
-else:
-    passive_phase_df.to_csv(os.path.join(root_path,'data',design_variant,'all_passive_phase_data.csv'),sep='\t')
-    active_phase_df.to_csv(os.path.join(root_path,'data',design_variant,'all_active_phase_data.csv'),sep='\t')
-    scipy.io.savemat(os.path.join(os.path.join(root_path,'data',design_variant,'all_active_phase_data.mat')),datadict,oned_as='row')
-    np.savez(os.path.join(os.path.join(root_path,'data',design_variant,'all_active_phase_data.mat.npz')),datadict = datadict)
+            ##.mat
+            subject_df.loc[np.isnan(subject_df['indif_eta']), 'selected_side_map'] = np.nan
 
+            #Retrieve growth rates
+            datadict.setdefault(f'gr1_1{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['gamma_left_up']))
+            datadict.setdefault(f'gr1_2{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['gamma_left_down']))
+            datadict.setdefault(f'gr2_1{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['gamma_right_up']))
+            datadict.setdefault(f'gr2_2{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['gamma_right_down']))
+
+            #Retrieve wealth changes
+            datadict.setdefault(f'x1_1{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['x1_1']))
+            datadict.setdefault(f'x1_2{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['x1_2']))
+            datadict.setdefault(f'x2_1{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['x2_1']))
+            datadict.setdefault(f'x2_2{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['x2_2']))
+
+            #Retrive wealth
+            datadict.setdefault(f'wealth{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['wealth']))
+
+            #Retrieve keypresses
+            datadict.setdefault(f'choice{CONDITION_SPECS["txt_append"][c]}',[]).append(np.array(subject_df['selected_side_map']))
+
+
+
+        active_phase_df.to_csv(os.path.join(root_path, 'all_active_phase_data.csv'), sep='\t')
+        scipy.io.savemat(os.path.join(root_path, 'all_active_phase_data.mat'), datadict, oned_as='row')
+        np.savez(os.path.join(root_path, 'all_active_phase_data.mat.npz'), datadict=datadict)
+
+        if passive_phase_df is not None:
+            passive_phase_df.to_csv(os.path.join(root_path, 'all_passive_phase_data.csv'), sep='\t')
+
+
+if __name__=='__main__':
+    simulation = True
+    design_variant = 'two_gamble_new_c'
+    n_passive_runs = 3
+
+    reading_data(simulation, design_variant, n_passive_runs)
