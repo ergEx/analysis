728942d3783a9d70b45cd61e6bf78968af18eeb0
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2023-02-16 10:46:33 +0000

    Bayesian parameter estimation: allow for different simulation settings

--- codebase/Bayesian_utils/setBayesian.m
+++ codebase/Bayesian_utils/setBayesian.m
@@ -1,4 +1,4 @@
-function setBayesian(inferenceMode,whichJAGS,whichQuals,doParallel,startDir,dataVersion)
+function setBayesian(inferenceMode,whichJAGS,whichQuals,doParallel,startDir,dataVersion, simVersion)
 
 % setHLM sets up multiple HLM models to run sequentially according to inputs
 
@@ -9,6 +9,10 @@ function setBayesian(inferenceMode,whichJAGS,whichQuals,doParallel,startDir,data
 % whichQuals    - sets the order of qualities to run
 % doParallel    - whether to run chains in parallel
 % dataVersion   - whether to run model on simulated data (1), pilot data (2) or full data (3)
+% simVersion    - if running on simulated data; low sensitivity,  low n  (1),
+%                                               high sensitivity, low n  (2),
+%                                               low sensitivity,  high n (3),
+%                                               high sensitivity, high n (4)
 
 %% Specifies qualities to be selected from
 numRuns      = length(whichQuals);     %how many separate instances of an MCMC to run
@@ -19,10 +23,17 @@ nThin        = 10;                     %thinnning factor, 1 = no thinning, 2=eve
 
 %% Specifies subjects, trials and directory_name
 switch dataVersion
-    case {1}, subjList = 1:9; nTrials = 160; folder = '0_simulation';  %Simulated data
-    case {2}, subjList = 1:11; nTrials = 160; folder = '1_pilot';   %Pilot data
-    case {3}, subjList = 1:1; nTrials = 1; dataVersion = '';   %Full experiment data
-end %version
+    case {1} %simulated data
+        subjList = 1:9;
+        switch simVersion
+            case {1},  nTrials = 160;  folder = '0_simulation/b_0_n_0';
+            case {2},  nTrials = 160;  folder = '0_simulation/b_1_n_0';
+            case {3},  nTrials = 1000; folder = '0_simulation/b_0_n_1';
+            case {4},  nTrials = 1000; folder = '0_simulation/b_1_n_1';
+        end %simVersion
+    case {2}, subjList = 1:11; nTrials = 160; folder = '1_pilot'; %Pilot data
+    case {3}, subjList = 1:1; nTrials = 1; folder = '2_full_data';%Full experiment data
+end %dataVersion
 
 %% Runs HLMs sequentially
 for i=1:numRuns
