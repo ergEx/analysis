50d42fc881c123ff4bca90dd2d7a9fd3181ed9d2
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2023-04-05 11:04:42 +0100

    Update on colors in plots

--- codebase/create_plots.py
+++ codebase/create_plots.py
@@ -11,8 +11,16 @@ import yaml
 from .base import get_config_filename
 
 
-def plot_trajectory(ax, df, c, title_dict={0: "Additive", 1: "Multiplicative"}, session="passive"):
-    ax.plot(df.trial, df.wealth)
+def get_colors(n):
+    cmap = plt.get_cmap("tab10")
+    colors = [cmap(i) for i in np.linspace(0, 1, n)]
+    return colors
+
+
+def plot_trajectory(
+    ax, df, c, color, title_dict={0: "Additive", 1: "Multiplicative"}, session="passive"
+):
+    ax.plot(df.trial, df.wealth, color=color)
     ax.set(title=title_dict[c], xlabel="Trial", ylabel="Wealth")
 
     if c == 1:
@@ -28,7 +36,7 @@ def plot_trajectory(ax, df, c, title_dict={0: "Additive", 1: "Multiplicative"}, 
     return ax
 
 
-def plot_passive(df_passive, fig_dir):
+def plot_passive(df_passive, fig_dir, colors):
     fig_passive, ax_passive = plt.subplots(2, 1)
     for i, participant in enumerate(set(df_passive.participant_id)):
         fig_passive_subject, ax_passive_subject = plt.subplots(2, 1)
@@ -37,8 +45,8 @@ def plot_passive(df_passive, fig_dir):
                 "participant_id == @participant and eta == @cond"
             ).reset_index(drop=True)
 
-            ax_passive[c] = plot_trajectory(ax_passive[c], df_tmp, c)
-            ax_passive_subject[c] = plot_trajectory(ax_passive_subject[c], df_tmp, c)
+            ax_passive[c] = plot_trajectory(ax_passive[c], df_tmp, c, colors[participant])
+            ax_passive_subject[c] = plot_trajectory(ax_passive_subject[c], df_tmp, c, color="grey")
 
         fig_passive_subject.suptitle(f"Wealth trajectory in learning task for participant {i+1}")
         fig_passive_subject.tight_layout()
@@ -48,7 +56,7 @@ def plot_passive(df_passive, fig_dir):
     fig_passive.savefig(os.path.join(fig_dir, f"0_1_passive"))
 
 
-def plot_active(df_active, fig_dir):
+def plot_active(df_active, fig_dir, colors):
     fig_active, ax_active = plt.subplots(2, 1)
     for i, participant in enumerate(set(df_active.participant_id)):
         fig_active_subject, ax_active_subject = plt.subplots(2, 1)
@@ -57,8 +65,12 @@ def plot_active(df_active, fig_dir):
                 "participant_id == @participant and eta == @cond"
             ).reset_index(drop=True)
 
-            ax_active[c] = plot_trajectory(ax_active[c], df_tmp, c, session="active")
-            ax_active_subject[c] = plot_trajectory(ax_active[c], df_tmp, c, session="active")
+            ax_active[c] = plot_trajectory(
+                ax_active[c], df_tmp, c, colors[participant], session="active"
+            )
+            ax_active_subject[c] = plot_trajectory(
+                ax_active_subject[c], df_tmp, c, color="grey", session="active"
+            )
 
         fig_active_subject.suptitle(f"Wealth trajectories in gamblig task for participant {i+1}")
         fig_active_subject.tight_layout()
@@ -68,7 +80,7 @@ def plot_active(df_active, fig_dir):
     fig_active.savefig(os.path.join(fig_dir, f"0_2_active"))
 
 
-def plot_raincloud(ax, df, c, pal, title_dict={0: "Additive", 1: "Multiplicative"}):
+def plot_raincloud(ax, df, c, title_dict={0: "Additive", 1: "Multiplicative"}):
     pt.RainCloud(
         x="tmp",
         hue="min_max",
@@ -77,7 +89,7 @@ def plot_raincloud(ax, df, c, pal, title_dict={0: "Additive", 1: "Multiplicative
         ax=ax,
         bw=0.3,
         orient="h",
-        palette=pal,
+        palette=["#e28743", "#043d74"],
         alpha=0.5,
         move=0.225,
         width_box=0.225,
@@ -88,12 +100,12 @@ def plot_raincloud(ax, df, c, pal, title_dict={0: "Additive", 1: "Multiplicative
     return ax
 
 
-def plot_indif_eta(df_active, pal, fig_dir):
+def plot_indif_eta(df_active, fig_dir):
     fig_indif_eta, ax_indif_eta = plt.subplots(2, 1)
     for c, cond in enumerate(set(df_active.eta)):
         df_tmp = df_active.query("eta == @cond").reset_index(drop=True)
 
-        ax_indif_eta[c] = plot_raincloud(ax_indif_eta[c], df_tmp, c, pal)
+        ax_indif_eta[c] = plot_raincloud(ax_indif_eta[c], df_tmp, c)
 
     fig_indif_eta.suptitle("Descriptive indifference eta")
     fig_indif_eta.tight_layout()
@@ -105,7 +117,7 @@ def plot_indif_eta(df_active, pal, fig_dir):
                 "participant_id == @participant and eta == @cond"
             ).reset_index(drop=True)
 
-            ax_indif_eta_subject[c] = plot_raincloud(ax_indif_eta_subject[c], df_tmp, c, pal)
+            ax_indif_eta_subject[c] = plot_raincloud(ax_indif_eta_subject[c], df_tmp, c)
 
         fig_indif_eta_subject.suptitle(f"Desctriptive indifference eta for participant {i+1}")
         fig_indif_eta_subject.tight_layout()
@@ -128,9 +140,8 @@ def log_reg_plot(ax, df, est, c, title_dict={0: "Additive", 1: "Multiplicative"}
         df.x_test, df.pred, color="black",
     )
 
-    ax.axvline(x=float(est.item()), ymax=0.5, linestyle="--", color="red", label="estimate")
+    ax.axvline(x=float(est.item()), ymax=0.5, linestyle="--", color="black", label="prediction")
     ax.axhline(y=0.5, linestyle="--", color="grey")
-
     ax.set(
         title=title_dict[c],
         xlabel="Indifference eta",
@@ -143,7 +154,7 @@ def log_reg_plot(ax, df, est, c, title_dict={0: "Additive", 1: "Multiplicative"}
     return ax
 
 
-def plot_regplot(ax, df, v, label_dict={0: "Lower bound", 1: "Upper bound"}):
+def plot_regplot(ax, df, v, color, label_dict={0: "Lower bound", 1: "Upper bound"}):
     sns.regplot(
         x=np.array(df[df.min_max == v].indif_eta),
         y=np.array(df[df.min_max == v].min_max),
@@ -151,7 +162,7 @@ def plot_regplot(ax, df, v, label_dict={0: "Lower bound", 1: "Upper bound"}):
         y_jitter=0.05,
         ax=ax,
         label=label_dict[v],
-        scatter_kws={"alpha": 0.5, "s": 3},
+        scatter_kws={"alpha": 0.5, "s": 3, "color": color},
     )
 
     return ax
@@ -167,13 +178,11 @@ def plot_log_reg(df, df_logistic, df_overview, fig_dir, data_variant):
             tmp = df_logistic.loc[idx["all", phenotype, con, :]]
             est = df_overview.loc[idx["all", phenotype, con]]
 
-            ax_log_reg[c] = log_reg_plot(
-                ax_log_reg[c], tmp, est.log_reg_decision_boundary, c, fig_dir
-            )
+            ax_log_reg[c] = log_reg_plot(ax_log_reg[c], tmp, est.log_reg_decision_boundary, c)
             df_tmp = df.query("phenotype == @phenotype and eta == @con")
 
-            ax_log_reg[c] = plot_regplot(ax_log_reg[c], df_tmp, 1)
-            ax_log_reg[c] = plot_regplot(ax_log_reg[c], df_tmp, 0)
+            ax_log_reg[c] = plot_regplot(ax_log_reg[c], df_tmp, 1, color="#043d74")
+            ax_log_reg[c] = plot_regplot(ax_log_reg[c], df_tmp, 0, color="#e28743")
 
             ax_log_reg[c].legend(loc="upper left", fontsize="xx-small")
         fig_log_reg.suptitle(f"Logistic regression {phenotype}")
@@ -190,12 +199,16 @@ def plot_log_reg(df, df_logistic, df_overview, fig_dir, data_variant):
                     tmp = df_logistic.loc[idx[participant, phenotype, con, :]]
                     est = df_overview.loc[idx[participant, phenotype, con]]
                     ax_log_reg_subject[c] = log_reg_plot(
-                        ax_log_reg_subject[c], tmp, est.log_reg_decision_boundary, c, fig_dir
+                        ax_log_reg_subject[c], tmp, est.log_reg_decision_boundary, c
                     )
                     df_tmp = df.query("participant_id == @participant and eta == @con")
 
-                    ax_log_reg_subject[c] = plot_regplot(ax_log_reg_subject[c], df_tmp, 1)
-                    ax_log_reg_subject[c] = plot_regplot(ax_log_reg_subject[c], df_tmp, 0)
+                    ax_log_reg_subject[c] = plot_regplot(
+                        ax_log_reg_subject[c], df_tmp, 1, color="#043d74"
+                    )
+                    ax_log_reg_subject[c] = plot_regplot(
+                        ax_log_reg_subject[c], df_tmp, 0, color="#e28743"
+                    )
 
                     ax_log_reg[c].legend(loc="upper left", fontsize="xx-small")
 
@@ -211,7 +224,12 @@ def plot_log_reg(df, df_logistic, df_overview, fig_dir, data_variant):
 
 
 def plot_bayesian(
-    df, df_bayesian, fig_dir, data_variant, legend_dict={0: "Additive", 1: "Multiplicative"}
+    df,
+    df_bayesian,
+    fig_dir,
+    data_variant,
+    legend_dict={0: "Additive", 1: "Multiplicative"},
+    colors={0: "blue", 1: "red"},
 ):
     # GROUP LEVEL
     for p, phenotype in enumerate(set(df.phenotype)):
@@ -220,14 +238,17 @@ def plot_bayesian(
             idx = pd.IndexSlice
             tmp = df_bayesian.loc[idx["all", phenotype, con, :]]
             ax_bayesian = sns.kdeplot(
-                tmp.samples.astype(float), ax=ax_bayesian, label=legend_dict[c], fill=True,
+                tmp.samples.astype(float),
+                ax=ax_bayesian,
+                label=legend_dict[c],
+                fill=True,
+                color=colors[c],
             )
             xs, ys = ax_bayesian.collections[-1].get_paths()[0].vertices.T
-            ax_bayesian.fill_between(xs, ys, color="red", alpha=0.05)
+            ax_bayesian.fill_between(xs, ys, color=colors[c], alpha=0.05)
             mode_idx = np.argmax(ys)
-            ax_bayesian.vlines(
-                xs[mode_idx], 0, ys[mode_idx], ls="--", color="red", label="Prediction"
-            )
+            ax_bayesian.vlines(xs[mode_idx], 0, ys[mode_idx], ls="--", color="black")
+        ax_bayesian.plot([], ls="--", color="black", label="Predictions")
         ax_bayesian.legend(loc="upper left", fontsize="xx-small")
         ax_bayesian.set(
             title=f"Bayesian parameter estimation {phenotype}",
@@ -251,13 +272,15 @@ def plot_bayesian(
                         ax=ax_bayesian_subjects,
                         label=legend_dict[c],
                         fill=True,
+                        color=colors[c],
                     )
                     xs, ys = ax_bayesian_subjects.collections[-1].get_paths()[0].vertices.T
-                    ax_bayesian_subjects.fill_between(xs, ys, color="red", alpha=0.05)
+                    # ax_bayesian_subjects.fill_between(xs, ys, color="red", alpha=0.05)
                     mode_idx = np.argmax(ys)
                     ax_bayesian_subjects.vlines(
-                        xs[mode_idx], 0, ys[mode_idx], ls="--", color="red", label="Prediction"
+                        xs[mode_idx], 0, ys[mode_idx], ls="--", color="black"
                     )
+                ax_bayesian_subjects.plot([], ls="--", color="black", label="Predictions")
                 ax_bayesian_subjects.legend(loc="upper left", fontsize="xx-small")
                 ax_bayesian_subjects.set(
                     title=f"Bayesian parameter estimation for participant {i} {phenotype}",
@@ -271,7 +294,7 @@ def plot_bayesian(
                 )
 
 
-def plot_heatmaps(df_bayesian, fig_dir, data_variant):
+def plot_heatmaps(df_bayesian, fig_dir, data_variant, colors):
     df_bayesian = df_bayesian[df_bayesian.index.get_level_values(0) != "all"]
     df_dynamics = df_bayesian.unstack(level="dynamic")
     df_dynamics.columns = ["_".join(map(str, col)).strip() for col in df_dynamics.columns.values]
@@ -282,18 +305,19 @@ def plot_heatmaps(df_bayesian, fig_dir, data_variant):
     df_dynamics["samples_1.0"] = df_dynamics["samples_1.0"].astype(float)
 
     hue = "participant" if data_variant != "0_simulation" else "phenotype"
-    test = sns.jointplot(
+    bayesian_heatmap = sns.jointplot(
         data=df_dynamics,
         x="samples_0.0",
         y="samples_1.0",
         hue=hue,
+        palette=sns.color_palette(colors),
         kind="kde",
         alpha=0.7,
         fill=True,
         xlim=[-0.5, 1.5],
         ylim=[-0.5, 1.5],
     )
-    test.savefig(os.path.join(fig_dir, f"0_6_bayesian_heatmap.png"))
+    bayesian_heatmap.savefig(os.path.join(fig_dir, f"0_6_bayesian_heatmap.png"))
 
 
 def main(config_file, i, simulation_variant=""):
@@ -311,9 +335,6 @@ def main(config_file, i, simulation_variant=""):
     if not os.path.isdir(fig_dir):
         os.makedirs(fig_dir)
 
-    colors = config["colors"]
-    pal = sns.set_palette(sns.color_palette(colors))
-
     if data_variant != "0_simulation":
         df_passive = pd.read_csv(os.path.join(data_dir, "all_passive_phase_data.csv"), sep="\t")
 
@@ -326,6 +347,8 @@ def main(config_file, i, simulation_variant=""):
 
     df_logistic = pd.read_pickle(os.path.join(data_dir, "plotting_files", "logistic.pkl"))
 
+    colors = get_colors(len(set(df_active.participant_id)))
+
     try:
         df_bayesian = pd.read_pickle(os.path.join(data_dir, "plotting_files", "bayesian.pkl"))
         run_bayesian = True
@@ -336,16 +359,16 @@ def main(config_file, i, simulation_variant=""):
         )
 
     if data_variant != "0_simulation":
-        plot_passive(df_passive, fig_dir)
+        plot_passive(df_passive, fig_dir, colors)
 
-        plot_active(df_active, fig_dir)
+        plot_active(df_active, fig_dir, colors)
 
-        plot_indif_eta(df_active, pal, fig_dir)
+        plot_indif_eta(df_active, fig_dir)
 
     plot_log_reg(df_active, df_logistic, df_overview, fig_dir, data_variant)
 
     if run_bayesian:
         plot_bayesian(df_active, df_bayesian, fig_dir, data_variant)
 
-        plot_heatmaps(df_bayesian, fig_dir, data_variant)
+        plot_heatmaps(df_bayesian, fig_dir, data_variant, colors)
 
