c65fb781b175d8053664d792eb58466949b89f9d
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2023-06-06 15:17:46 +0100

    Add jasp input to pipeline

--- codebase/create_JASP_input.py
+++ codebase/create_JASP_input.py
@@ -11,44 +11,52 @@ from .experiment_specs import condition_specs, sub_specs
 from .utils import read_Bayesian_output
 
 
-def read(save_path):
-    bayesian_parameter_estimation_output_file = os.path.join(
-        save_path, "Bayesian_parameter_estimation.mat"
-    )
-    if os.path.isfile(bayesian_parameter_estimation_output_file):
-        bayesian_samples = read_Bayesian_output(bayesian_parameter_estimation_output_file)
-    else:
-        bayesian_samples = None
-        print("Bayesian parameter estimation output not found!")
-    return bayesian_samples
-
-
 def extract(subjects, condition_specs, bayesian_samples):
-    data = {"0.0": [None] * len(subjects), "1.0": [None] * len(subjects)}
-    for j, subject1 in enumerate(subjects):
-        for c, condition in enumerate(condition_specs["lambd"]):
-            try:
-                eta_dist = bayesian_samples["eta"][:, :, j, c].flatten()
-                kde = sm.nonparametric.KDEUnivariate(eta_dist).fit()
-                data[f"{c}.0"][j] = kde.support[np.argmax(kde.density)]
-            except Exception as e:
-                pass
+
     return pd.DataFrame.from_dict(data)
 
 
-def main(config_file, i=0, simulation_variant=""):
+def main(config_file):
+
+    if not config["create JASP file"]["run"]:
+        return
+
+    print(f"\nCREATING JASP FILE")
+
     with open(config_file, "r") as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
-    save_path = config["data_folders"][0]
-    run_stage = config["create_jasp"]["run"]
 
-    if run_stage:
-        SUBJECT_SPECS = sub_specs(config["data_variant"])
-        subjects = SUBJECT_SPECS["id"]
-        CONDITION_SPECS = condition_specs()
-        bayesian_samples = read(save_path)
-        c_bayesian = extract(subjects, CONDITION_SPECS, bayesian_samples)
-        c_bayesian.to_csv(os.path.join(save_path, "jasp_input.csv"), sep="\t")
+    data_dir = config["data directoty"]
+
+    SUBJECT_SPECS = sub_specs(config["data_variant"])
+    subjects = SUBJECT_SPECS["id"]
+    CONDITION_SPECS = condition_specs()
+
+    data = {"0.0_partial_pooling": [None] * len(subjects), "1.0_partial_pooling": [None] * len(subjects),
+            "0.0_no_pooling": [None] * len(subjects), "1.0_no_pooling": [None] * len(subjects)}
+
+    #bracketing method
+    bracketing_overview = pd.read_csv(os.path.join(data_dir, "bracketing_overview.csv"), sep = '\t')
+    data['0.0_bracketing'] = bracketing_overview.query('participant != "all" and dynamic == 0.0').reset_index(drop=True).log_reg_decision_boundary
+    data['1.0_bracketing'] = bracketing_overview.query('participant != "all" and dynamic == 1.0').reset_index(drop=True).log_reg_decision_boundary
+
+    #bayesian method
+    for pool in ['no_pooling', 'partial_pooling']:
+        bayesian_samples = read_Bayesian_output(
+                    os.path.join(data_dir, f"Bayesian_JAGS_parameter_estimation_{pool}.mat")
+                )
+        for j, subject1 in enumerate(subjects):
+            for c, condition in enumerate(CONDITION_SPECS["lambd"]):
+                try:
+                    eta_dist = bayesian_samples["eta"][:, :, j, c].flatten()
+                    kde = sm.nonparametric.KDEUnivariate(eta_dist).fit()
+                    data[f"{c}.0_{pool}"][j] = kde.support[np.argmax(kde.density)]
+                except Exception as e:
+                    pass
+
+    df = pd.DataFrame.from_dict(data)
+
+    df.to_csv(os.path.join(data_dir, "jasp_input.csv"), sep="\t")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import sys
 import time
 
 import yaml
-from codebase import base, bracketing_method, create_plots, readingdata
+from codebase import base, bracketing_method, create_JASP_input, create_plots, readingdata
+
+
+def bayesian_method(config):
+    if config['bayesian method']['run']:
+        response = input(f'\nThe Bayesian models are run from "runBayesian.sh". \nTo run these you must therefore quit this pipeline and run it seperately. \nDo you want to continue without running the Bayesian models? ([y]/n): ').lower()
+        if response == 'n' or response == 'no':
+            sys.exit()
+        else:
+            print('Continuing without re-running the Bayesian models')
 
 
 def main():
     config_file = base.get_config_filename(sys.argv)
+    #config_file = 'config_1_pilot.yaml'
 
     with open(f"{config_file}", "r") as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
@@ -20,13 +30,9 @@ def main():
     print(f"Data: {data_type} \nVariant: {data_variant}")
 
     readingdata.main(config_file)
-    if config['bayesian method']['run']:
-        response = input(f'\nThe Bayesian models are run from "runBayesian.sh". \nTo run these you must therefore quit this pipeline and run it seperately. \nDo you want to continue without running the Bayesian models? ([y]/n): ').lower()
-        if response == 'n' or response == 'no':
-            sys.exit()
-        else:
-            print('Continuing without re-running the Bayesian models')
+    bayesian_method(config)
     bracketing_method.main(config_file)
+    create_JASP_input.main(config_file)
     create_plots.main(config_file)
 
     print(f"\n--- Code ran in {(time.time() - start_time):.2f} seconds ---")
