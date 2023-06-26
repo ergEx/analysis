1189a05409c7457b314c4e2e7be8bf65c94d2d28
Benjamin Skjold <54884346+benj1003@users.noreply.github.com>
2023-06-26 16:12:13 +0200

    Updating comments

--- bayes factor design analysis/ergEx_RR_NHB.R
+++ bayes factor design analysis/ergEx_RR_NHB.R
@@ -22,12 +22,12 @@ bfbound <- 10
 nsims <- 400
 ntraj <- 100
 
-#H0: eta_mul isnot> eta_add
+#H0: d_h1 >= d_h0
 sim.H0 <- BFDA.sim(expected.ES=0.0, type="t.paired", prior=list("Cauchy", list(prior.location=0, prior.scale=sqrt(2)/2)), n.min=nmin, n.max=nmax, alternative="greater", boundary=Inf, B=nsims, verbose=TRUE, cores=cores-1, stepsize = nstep)
 BFDA.analyze(sim.H0, design="sequential", n.min=nmin, n.max=nmax, boundary=bfbound)
 plot(sim.H0, n.min=50, n.max=nmax, boundary=bfbound, n.trajectories = ntraj)
 
-#H1: eta_mul is > eta_add, (using 95% lower bound of effect size from pilot)
+#H1: d_h1 < d_h0
 sim.H1 <- BFDA.sim(expected.ES=0.5, type="t.paired", prior=list("Cauchy", list(prior.location=0, prior.scale=sqrt(2)/2)), n.min=nmin, n.max=nmax, alternative="greater", boundary=Inf, B=nsims, verbose=TRUE, cores=cores-1, stepsize = nstep)
 BFDA.analyze(sim.H1, design="sequential", n.min=nmin, n.max=nmax, boundary=bfbound)
 plot(sim.H1, n.min=50, n.max=nmax, boundary=bfbound, n.trajectories = ntraj)
