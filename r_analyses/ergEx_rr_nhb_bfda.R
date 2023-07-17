1af15f21a2cc554f2a171e0e1349fb20347703f1
SRSteinkamp <simon.steinkamp@googlemail.com>
2023-07-17 13:43:55 +0200

    renamed plot and bfda

--- r_analyses/ergEx_rr_nhb_bfda.R
+++ r_analyses/ergEx_rr_nhb_bfda.R
@@ -17,7 +17,7 @@ nmin <- 25
 nmax <- 150
 nstep <- 1
 bfbound <- 10
-nsims <- 10000
+nsims <- 2500
 ntraj <- 250
 filename <- 'bfda_log.txt'
 simH0_file <- 'simH0.RData'
@@ -37,7 +37,7 @@ sink(file = filename, type = c("output", "message"), split = FALSE)
 BFDA.analyze(sim.H0, design="sequential", n.min=nmin, n.max=nmax, boundary=bfbound)
 sink()
 
-pdf('plot1.pdf', width=10, height=10)
+pdf('plot_bfda_h0.pdf', width=10, height=10)
 print(plot(sim.H0, n.min=50, n.max=nmax, boundary=bfbound, n.trajectories = ntraj))
 dev.off()
 
@@ -53,12 +53,12 @@ sink(file = filename, type = c("output", "message"), split = FALSE, append=TRUE)
 BFDA.analyze(sim.H1, design="sequential", n.min=nmin, n.max=nmax, boundary=bfbound)
 sink()
 
-pdf('plot2.pdf', width=10, height=10)
+pdf('plot_bfda_h1.pdf', width=10, height=10)
 print(plot(sim.H1, n.min=50, n.max=nmax, boundary=bfbound, n.trajectories = ntraj))
 dev.off()
 
 #analyse threshold hitting events 
-evDens(BFDA.H0=sim.H0, BFDA.H1=sim.H1, n=nsims, boundary=c(1/bfbound, bfbound))
+# evDens(BFDA.H0=sim.H0, BFDA.H1=sim.H1, n=nsims, boundary=c(1/bfbound, bfbound))
 
 #sample size determination
 sink(file = filename, type = c("output", "message"), split = FALSE, append=TRUE)
