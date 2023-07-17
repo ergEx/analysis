bc0bd7693de8641a87dbf7fd53b24621d60beca9
SRSteinkamp <simon.steinkamp@googlemail.com>
2023-07-17 10:51:28 +0200

    storing bfda outputs to text.

--- r_analyses/ergEx_rr_nhb_bfda.R
+++ r_analyses/ergEx_rr_nhb_bfda.R
@@ -13,41 +13,53 @@ cl <- makeCluster(cores - 1)
 registerDoParallel(cl)
 
 # Clear console and plot windows
-#cat("\014")
-#dev.off()
+cat("\014")
+dev.off()
 
-#Initialise variables
-nmin <- 20
+# Initialise variables
+nmin <- 25
 nmax <- 150
-nstep <- 2
+nstep <- 1
 bfbound <- 10
-nsims <- 10
-ntraj <- 1
+nsims <- 10000
+ntraj <- 250
+filename <- 'bfda_log.txt'
+
 
 #H0: d_h1 >= d_h0
 sim.H0 <- BFDA.sim(expected.ES=0.0, type="t.paired", prior=list("Cauchy", list(prior.location=0, prior.scale=sqrt(2)/2)), n.min=nmin, n.max=nmax, 
-                   alternative="greater", boundary=1/10, B=nsims, verbose=TRUE, cores=cores-1, stepsize = nstep)
+                   alternative="greater", boundary=Inf, B=nsims, verbose=TRUE, cores=cores-1, stepsize = nstep, design = 'sequential')
+
+sink(file = filename, type = c("output", "message"), split = FALSE)
 BFDA.analyze(sim.H0, design="sequential", n.min=nmin, n.max=nmax, boundary=bfbound)
+sink()
 
 pdf('plot1.pdf', width=10, height=10)
 print(plot(sim.H0, n.min=50, n.max=nmax, boundary=bfbound, n.trajectories = ntraj))
 dev.off()
 
 #H1: d_h1 < d_h0
+
 sim.H1 <- BFDA.sim(expected.ES=0.5, type="t.paired", prior=list("Cauchy", list(prior.location=0, prior.scale=sqrt(2)/2)), n.min=nmin, n.max=nmax, 
-                   alternative="greater", boundary=010, B=nsims, verbose=TRUE, cores=cores-1, stepsize = nstep)
+                   alternative="greater", boundary=Inf, B=nsims, verbose=TRUE, cores=cores-1, stepsize = nstep, design = 'sequential')
+
+sink(file = filename, type = c("output", "message"), split = FALSE, append=TRUE)
 BFDA.analyze(sim.H1, design="sequential", n.min=nmin, n.max=nmax, boundary=bfbound)
+sink()
 
 pdf('plot2.pdf', width=10, height=10)
 print(plot(sim.H1, n.min=50, n.max=nmax, boundary=bfbound, n.trajectories = ntraj))
 dev.off()
 
 #analyse threshold hitting events 
-evDens(BFDA.H0=sim.H0, BFDA.H1=sim.H1, n=nsims, boundary=c(1/bfbound, bfbound))
+#evDens(BFDA.H0=sim.H0, BFDA.H1=sim.H1, n=nsims, boundary=c(1/bfbound, bfbound))
 
 #sample size determination
+sink(file = filename, type = c("output", "message"), split = FALSE, append=TRUE)
 SSD(sim.H0, alpha=.05, boundary=c(10))
 SSD(sim.H1, power=.90, boundary=c(10))
+sink()
 
+unlink(filename)
 # Stop parallel processing
 stopCluster(cl)
