d4aed4d92af8297d5297ee0177ed65fcfbc5b4d6
SRSteinkamp <simon.steinkamp@googlemail.com>
2023-09-06 15:29:27 +0200

    changed quality for qual1 to worse

--- codebase/Bayesian_utils/setBayesian.m
+++ codebase/Bayesian_utils/setBayesian.m
@@ -21,9 +21,9 @@ function setBayesian(dataSource,simVersion,dataPooling,inferenceMode,model_selec
 
 %% Specifies qualities to be selected from
 numRuns      = length(dataPooling);     %how many separate instances of an MCMC to run
-nBurnin      = [1e2,1e3,1e4,2e4,4e4];  %from 100 to 40k
+nBurnin      = [1e1,1e3,1e4,2e4,4e4];  %from 100 to 40k
 nSamples     = [5e1,5e2,5e3,1e4,2e4];  %from 50 to 20k
-nChains      = [4,4,4,4,4];            %Keep this to 4
+nChains      = [2,4,4,4,4];            %Keep this to 4
 nThin        = 10;                     %thinnning factor, 1 = no thinning, 2=every 2nd etc.
 
 %% Specifies subjects, trials and directory_name
