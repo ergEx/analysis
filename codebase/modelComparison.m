5eb651d93cec47c1c4e375601c2fee1f4f203416
SRSteinkamp <simon.steinkamp@googlemail.com>
2023-09-06 14:51:39 +0200

    model comparison further work

--- codebase/modelComparison.m
+++ codebase/modelComparison.m
@@ -1,27 +1,34 @@
-function modelComparison(data_source, data_dir, model_selection_type)
+function modelComparison(data_source, model_selection_type)
 
 
 %data_source = '/1_pilot';
 %model_selection_type = 1;
 
-%[startDir,~] = fileparts(mfilename('fullpath'));  %specify your starting directory here (where this script runs from)
+[startDir,~] = fileparts(mfilename('fullpath'));  %specify your starting directory here (where this script runs from)
 %addpath(genpath(fullfile(startDir,'/VBA-toolbox')));
 
+base = pwd;
+[vba_dir, ~ ] = fileparts(which('VBA_setup'));
+cd(vba_dir)
+VBA_setup;
+cd(base)
+
 data_poolings = {'no_pooling','partial_pooling','full_pooling'};
-%dataDir=fullfile(startDir,'..','/data',data_source);
+dataDir=fullfile(startDir,'..','/data',data_source);
+figDir = fullfile(startDir, '..', '/figs', data_source)
 
 for ii = 1:length(data_poolings)
     file = sprintf('Bayesian_JAGS_model_selection_%s_%d.mat', data_poolings{ii} ,model_selection_type);
     
-    load(fullfile(dataDir, file))
+    jags_dat = load(fullfile(dataDir, file))
     
-    z = samples.z;
+    z = jags_dat.samples.z;
     [n_chains, n_samples, n_participants] = size(z);
     z_i = reshape(z, [n_chains * n_samples, n_participants]);
     
-    n_models = max(arr(:));
+    n_models = max(z(:));
     
-    counts = zeros(n, n_participants);
+    counts = zeros(n_models, n_participants);
     bin_edges = 1:(n_models+1);
     
     % Loop through each column and count occurrences
@@ -33,14 +40,14 @@ for ii = 1:length(data_poolings)
     counts = counts + 1; % Add 1 to all counts to avoid division by zero
     
     total_counts = sum(counts, 1);
-    proportions = counts ./ repmat(total_counts, n, 1);
+    proportions = counts ./ repmat(total_counts, n_models, 1);
     
     log_proportions = log10(proportions);
-    
+    options = {};
     % perform group-BMS on data
     [p1, o1] = VBA_groupBMC (log_proportions, options);
     set (o1.options.handles.hf, 'name', 'group BMS: y_1')
-     
+    saveas(gcf, fullfile(figDir, sprintf('model_selection_%s_%i.pdf', ii, model_selection_type)));
 end
 
 end
