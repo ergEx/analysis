e835de7bb3eadc7a400f07b677d3bb71891098b6
SRSteinkamp <simon.steinkamp@googlemail.com>
2023-09-06 15:30:19 +0200

    finalizing step 5

--- step5_model_comparison.py
+++ step5_model_comparison.py
@@ -4,7 +4,7 @@ import yaml
 import subprocess
 from codebase import readingdata, utils
 
-VBA_PATH = ''
+VBA_PATH = '/mnt/projects/CPM/toolboxes/VBA-toolbox'
 
 def main():
     config_file = utils.get_config_filename(sys.argv)
@@ -15,19 +15,27 @@ def main():
     data_type = config["data_type"]
     data_variant = config["data_variant"]
 
+    if sys.argv[2] == '1':
+        model_selection_type = '1'
+    elif sys.argv[2] == '2':
+        model_selection_type = '2'
+    else:
+        raise ValueError("Model selection type has to be 1 for source or 2 for sbatch.")
+
     start_time = time.time()
     print(f"\n--- {time.ctime(start_time)} --- ")
     print(f"\Doing model comparison no:")
     print(f"Data: {data_type} \nVariant: {data_variant}")
-    shellparams = {'VBA_PATH': VBA_PATH}
+    shellparams = {'VBA_PATH': VBA_PATH, 'data_variant' : data_variant, 
+                   'model_selection_type': model_selection_type}
     matlab_call = """
     module load matlab;
-    matlab -nodesktop -nojvm -nosplash -r\
+    matlab -nodesktop  -r\
     "addpath('codebase/'); addpath('{VBA_PATH}');\
-    modelComparison(); exit;"
+    modelComparison('{data_variant}', {model_selection_type}); exit;"
     """.format(**shellparams)
 
-    print(matlab_call)
+    subprocess.call(matlab_call, shell=True)
  
     print(f"\n--- Code ran in {(time.time() - start_time):.2f} seconds ---")
 
@@ -41,6 +49,6 @@ if __name__ == "__main__":
     write_provenance(command)
     try:
         main()
-    #   write_provenance('executed successfully')
+        write_provenance('executed successfully')
     except:
         write_provenance('FAILED!!')
