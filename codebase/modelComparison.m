f3cf10d328a1f77cfb99b38256d66bb4f784aac1
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2023-09-06 16:34:24 +0200

    Saving BF for model comparison

--- codebase/modelComparison.m
+++ codebase/modelComparison.m
@@ -17,8 +17,11 @@ data_poolings = {'no_pooling','partial_pooling','full_pooling'};
 dataDir=fullfile(startDir,'..','/data',data_source);
 figDir = fullfile(startDir, '..', '/figs', data_source)
 
+
+
 for ii = 1:length(data_poolings)
     file = sprintf('Bayesian_JAGS_model_selection_%s_%d.mat', data_poolings{ii} ,model_selection_type);
+    BFFile = sprintf('model_selection_BF_%s_%d.txt', data_poolings{ii} ,model_selection_type);
     
     jags_dat = load(fullfile(dataDir, file))
     
@@ -43,6 +46,31 @@ for ii = 1:length(data_poolings)
     proportions = counts ./ repmat(total_counts, n_models, 1);
     
     log_proportions = log10(proportions);
+    
+    BF10 = exp(sum(log_proportions(2, :)) - sum(log_proportions(1, :)));
+    fprintf(num2str(sum(log_proportions(2, :))))
+    fprintf(num2str(sum(log_proportions(1, :))))
+    fprintf(num2str(exp(sum(log_proportions(2, :)))))
+    fprintf(num2str(exp(sum(log_proportions(1, :)))))
+    BF = ['BF10 ', num2str(BF10)];
+    
+    if n_models == 3
+        BF20 = exp(sum(log_proportions(3, :)) - sum(log_proportions(1, :)));
+        BF21 = exp(sum(log_proportions(3, :)) - sum(log_proportions(2, :)));
+        BF = [BF, '\n', 'BF20 ', num2str(BF20), '\n', 'BF21 ', num2str(BF21)];
+    end
+    
+    if exist(fullfile(dataDir, BFFile), 'file')
+        fileID = fopen(fullfile(dataDir, BFFile), 'w');
+        fprintf(fileID, BF);
+        fclose(fileID);
+    else
+        fileID = fopen(fullfile(dataDir, BFFile), 'w');
+        fprintf(fileID, BF);
+        fclose(fileID);
+    end
+    
+    
     options = {};
     % perform group-BMS on data
     [p1, o1] = VBA_groupBMC (log_proportions, options);
