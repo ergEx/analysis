09c8809e3dde32f188d80467f93cd779a48c50a7
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2023-12-14 14:48:20 +0000

    variable inconcistency bug

--- codebase/create_plots.py
+++ codebase/create_plots.py
@@ -212,7 +212,7 @@ def main(config_file):
         bayesian_samples_partial_pooling = read_Bayesian_output(
                     os.path.join(data_dir, "Bayesian_JAGS_parameter_estimation_partial_pooling.mat")
                 )
-        eta_group = bayesian_samples_partial_pooling["eta_g"]
+        eta_g = bayesian_samples_partial_pooling["eta_g"]
 
         fig, ax = plt.subplots(1, 1, figsize=(23 * cm, 4.75 * cm))
         ax.axvspan(0, burn_in, alpha=0.2, color='grey', label = 'Burn in')
