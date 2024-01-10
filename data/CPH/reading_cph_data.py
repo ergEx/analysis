a71bd41a58a7a2165490994d1cf26501b2e4cfe2
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-01-10 11:32:15 +0100

    data loading now loads data

--- data/CPH/reading_cph_data.py
+++ data/CPH/reading_cph_data.py
@@ -4,6 +4,13 @@ import numpy as np
 import pandas as pd
 from scipy import io
 
+
+DATA_PATH = ''
+
+PARTICIPANTS = ['Subj01', 'Subj02', 'Subj03', 'Subj05', 'Subj06', 'Subj07', 'Subj08', 'Subj09',
+                'Subj10', 'Subj11', 'Subj12' ,'Subj13', 'Subj14', 'Subj15', 'Subj16', 'Subj17',
+                'Subj18', 'Subj19', 'Subj20']
+
 dfs = []
 
 selected_columns = ['subjID', 'earnings', 'Gam1_1', 'Gam1_2', 'Gam2_1', 'Gam2_2', 'KP_Final', 'eta']
@@ -12,33 +19,39 @@ renamed_columns = {'subjID': 'participant_id', 'earnings': 'wealth',
 value_mapping = {8.0: 0, 9.0: 1, np.nan: np.nan}
 datadict = {}
 
-excluded_participant = 5
+condition_map = {'add': 'Additive', 'mul': 'Mutiplicative'}
+
 min_n_trials = 299
 
 for condition in ['add', 'mul']:
-    files = [file for file in os.listdir(condition) if file.endswith(".txt")]
+    for sub in PARTICIPANTS:
 
-    for file in files:
-        file_path = os.path.join(condition, file)
-        df = pd.read_csv(file_path, delimiter='\t')
+        file = os.path.join(DATA_PATH, sub, condition_map[condition],
+                            'Data_active', f'{sub[-2:]}_2.txt')
 
-        if df.subjID.unique()[0] == excluded_participant:
-            continue
-        else:
+        if not os.path.isfile(file) and condition == 'mul':
+            file = os.path.join(DATA_PATH, sub, 'Multiplicative',
+                            'Data_active', f'{sub[-2:]}_2.txt')
 
-            df['eta'] = 1 if condition == 'mul' else 0
-            df = df[selected_columns].rename(columns=renamed_columns)
+        try:
+            df = pd.read_csv(file, delimiter='\t', header=0, error_bad_lines=True,
+                             usecols=range(0, 27))
+        except ValueError:
+            df = pd.read_csv(file, delimiter='\t', header=0, error_bad_lines=True,
+                             usecols=range(0, 25))
 
-            df['choice'] = df['selected_side'].map(value_mapping)
+        df['eta'] = 1 if condition == 'mul' else 0
+        df = df[selected_columns].rename(columns=renamed_columns)
 
-            df = df[:min_n_trials]
+        df['choice'] = df['selected_side'].map(value_mapping)
+        df = df[:min_n_trials]
 
-            dfs.append(df)
+        dfs.append(df)
 
-            for key in ['x1_1', 'x1_2', 'x2_1', 'x2_2', 'wealth', 'choice']:
-                datadict.setdefault(f'{key}_{condition}', []).append(
-                np.array(df[key])
-            )
+        for key in ['x1_1', 'x1_2', 'x2_1', 'x2_2', 'wealth', 'choice']:
+            datadict.setdefault(f'{key}_{condition}', []).append(
+            np.array(df[key])
+        )
 
 combined_df = pd.concat(dfs, ignore_index=True)
 
