f9c13047d327e4318dbcd0859c08f03bcf610fc3
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-01-11 10:15:11 +0100

    excluding particpant 5

--- data/CPH/reading_cph_data.py
+++ data/CPH/reading_cph_data.py
@@ -10,8 +10,9 @@ from codebase.utils import isoelastic_utility, wealth_change
 
 DATA_PATH = 'https://raw.githubusercontent.com/ollie-hulme/ergodicity-breaking-choice-experiment/master/data/'
 
-PARTICIPANTS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+PARTICIPANTS = ['1', '2', '3', '4', '6', '7', '8', '9', '10', '11',
                 '12', '13', '14', '15', '16', '17', '18', '19']
+# Subject 5 was excluded due to information on https://github.com/ollie-hulme/ergodicity-breaking-choice-experiment/tree/master
 
 dfs = []
 
