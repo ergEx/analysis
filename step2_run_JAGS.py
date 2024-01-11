1548527b8634e6982fa5b15eea52e32130f3cec5
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-01-11 10:17:33 +0100

    expanding data sources

--- step2_run_JAGS.py
+++ step2_run_JAGS.py
@@ -18,6 +18,10 @@ def make_shell(dataSource, inferenceMode, simVersion, quality, model_selection_t
         dataSource = 1
     elif dataSource == '2_full_data':
         dataSource = 2
+    elif dataSource == '2_full_data_excl':
+        dataSource = 3
+    elif dataSource == "CPH":
+        dataSource = 4
 
     shellname = (f'runBayes_ds-{dataSource}_im-{inferenceMode}_' +
                  f'sv-{simVersion}_q-{quality}_mst-{model_selection_type}.sh')
@@ -89,8 +93,10 @@ def main():
         inferenceMethod = 2
     elif sys.argv[2] == '3':
         inferenceMethod = 3
+    elif sys.argv[2] == '4':
+        inferenceMethod = 4
     else:
-        raise ValueError("Inference method, second argument has to be 1 or 2")
+        raise ValueError("Inference method, second argument has to be 1, 2, 3, or 4")
 
     if sys.argv[3] == '1':
         model_selection_type = '1'
@@ -146,5 +152,6 @@ if __name__ == "__main__":
     try:
         main()
         write_provenance('executed successfully')
-    except:
+    except Exception as e:
+        print(e)
         write_provenance('FAILED!!')
