4a2ae596ea51bf5d9489c5ad9aa9eafde2b52c55
SRSteinkamp <simon.steinkamp@googlemail.com>
2024-02-28 14:33:16 +0100

    letting bracketing fail if necessary

--- codebase/create_JASP_input.py
+++ codebase/create_JASP_input.py
@@ -37,11 +37,6 @@ def main(config_file):
     data = {"0.0_partial_pooling": [None] * len(subjects), "1.0_partial_pooling": [None] * len(subjects),
             "0.0_no_pooling": [None] * len(subjects), "1.0_no_pooling": [None] * len(subjects)}
 
-    #bracketing method
-    bracketing_overview = pd.read_csv(os.path.join(data_dir, "bracketing_overview.csv"), sep = '\t')
-    data['0.0_bracketing'] = bracketing_overview.query('participant != "all" and dynamic == 0.0').reset_index(drop=True).log_reg_decision_boundary
-    data['1.0_bracketing'] = bracketing_overview.query('participant != "all" and dynamic == 1.0').reset_index(drop=True).log_reg_decision_boundary
-
     #bayesian method
     for pool in ['no_pooling', 'partial_pooling']:
         bayesian_samples = read_Bayesian_output(
@@ -71,10 +66,18 @@ def main(config_file):
     df.insert(6, 'd_h1_no_pooling', d_h1)
     df.insert(7, 'd_h0_no_pooling', d_h0)
 
-    d_h1 = np.sqrt((df['0.0_bracketing'] - 0) ** 2 + (df['1.0_bracketing'] - 1) ** 2)
-    d_h0 = np.abs(df['0.0_bracketing'] - df['1.0_bracketing']) / np.sqrt(2)
-    df.insert(10, 'd_h1_bracketing', d_h1)
-    df.insert(11, 'd_h0_bracketing', d_h0)
+    try:
+        #bracketing method
+        bracketing_overview = pd.read_csv(os.path.join(data_dir, "bracketing_overview.csv"), sep = '\t')
+        data['0.0_bracketing'] = bracketing_overview.query('participant != "all" and dynamic == 0.0').reset_index(drop=True).log_reg_decision_boundary
+        data['1.0_bracketing'] = bracketing_overview.query('participant != "all" and dynamic == 1.0').reset_index(drop=True).log_reg_decision_boundary
+
+        d_h1 = np.sqrt((df['0.0_bracketing'] - 0) ** 2 + (df['1.0_bracketing'] - 1) ** 2)
+        d_h0 = np.abs(df['0.0_bracketing'] - df['1.0_bracketing']) / np.sqrt(2)
+        df.insert(10, 'd_h1_bracketing', d_h1)
+        df.insert(11, 'd_h0_bracketing', d_h0)
+    except FileNotFoundError:
+        print("Not using bracketing")
 
     df.to_csv(os.path.join(data_dir, "jasp_input.csv"), sep="\t")
 
